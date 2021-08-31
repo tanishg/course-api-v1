c0f3195040c18be4a43a1440a1cad789a7b5649a
Tanish Gupta <guptatanish9080@gmail.com>
2021-08-31 09:00:59 +0530

    added boot file

---
+++ src/main/io/tanishg/springbootstarter/CourseApiApp.java
@@ -0,0 +1,12 @@
+package io.tanishg.springbootstarter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
